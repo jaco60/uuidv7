11c6653a81f132fe2f4801498a6f07a064b1857d
jaco60 <jacoboni@univ-tlse2.fr>
2024-06-30 10:20:13 +0200

    Add subtype predicate to UUIDv7 type

--- src/uuid.ads
+++ src/uuid.ads
@@ -1,7 +1,9 @@
 package Uuid is
-   subtype UUIDv7 is String (1 .. 36);
-   function Generate_UUIDv7 return UUIDv7;
+  subtype UUIDv7 is String (1 .. 36) with
+    Dynamic_Predicate => (for all C of UUIDv7 => C in '0' .. '9' | 'a' .. 'f');
+
+  function Generate_UUIDv7 return UUIDv7;
 private
-   function To_Hex (Value : Integer) return Character;
-   function Random_Hex return Character;
+  function To_Hex (Value : Integer) return Character;
+  function Random_Hex return Character;
 end Uuid;
